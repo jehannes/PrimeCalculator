33c855aa7924c3fcf9018dc1361ab9cb4368399e
jehannes <37177127+jehannes@users.noreply.github.com>
2020-07-18 15:26:34 +0200

    added comment to primelib cpp

--- PrimeFactorizer/PrimeLibrary.cpp
+++ PrimeFactorizer/PrimeLibrary.cpp
@@ -62,7 +62,7 @@ void PrimeLibrary::LoadLib(string fname)
 				break;
 			}
 			else {
-				PrimeVect.push_back((unsigned long long)stoi(str));
+				PrimeVect.push_back((unsigned long long)stoi(str));//change to stoull or stoul
 			}
 		}
 
